e5f76e2b31add99b6150fb96e78e3c5b1e600ede
huff <huffinator@nerdwatchtower.com>
2025-05-20 18:40:46 -0700

    updated challenge

--- challenges/l6.challenge.js
+++ challenges/l6.challenge.js
@@ -3,8 +3,23 @@ let name = "Chris";
 let age = 37;
 let enjoysCoding = true;
 
+// preliminary log
+console.log(`Name: ${name}`)
+console.log(`Age: ${age}`)
+console.log(`Enjoys Coding? ${enjoysCoding}`)
+
 // arithmetic operators
 age++;
 let remainder = age % 5;
 
-//convert age to string
+// convert age to string
+let strAge = String(age);
+
+// am i older than 20?
+let olderThan20 = age > 20;
+
+// output to console
+console.log(`New Age: ${age}`)
+console.log(`Remainder: ${remainder}`)
+console.log(`Age as a string: ${strAge}`)
+console.log(`Am I older than 20? ${olderThan20}`)
